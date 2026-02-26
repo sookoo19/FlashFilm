db62053891b0f18dc14837737b2582cedb9a15b1
sookoo19 <kouhei20001011@gmail.com>
2026-02-26 21:12:45 +0100

    developブランチ用typesファイル復元

--- FlashFilm/src/types/camera.ts
+++ FlashFilm/src/types/camera.ts
@@ -1,6 +1,6 @@
 export type CapturedPhoto = {
-  uri: string;
   width: number;
   height: number;
   base64?: string;
+  uri: string;
 };
--- FlashFilm/src/types/navigation.ts
+++ FlashFilm/src/types/navigation.ts
@@ -1,8 +1,8 @@
 import type { CapturedPhoto } from './camera';
 
 export type RootStackParamList = {
-  Camera: undefined;
   ImageProcessing: {
     photo: CapturedPhoto;
   };
+  Camera: undefined;
 };
